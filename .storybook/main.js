8103fade4289cdfb755bfdacbb2715be7bc9f24f
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-11-06 12:39:49 +0200

    feat:🎸 storybook works

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,39 +1,48 @@
 import path from 'path';
 
 const config = {
-  stories: ['../src/app/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-links', {
-    name: path.dirname(require.resolve('@storybook/addon-docs/package.json')),
-    options: { transcludeMarkdown: true },
-  }, {
-    name: '@storybook/addon-essentials',
-    options: { docs: false },
-  }, '@storybook/addon-interactions', '@storybook/addon-docs', {
-    name: '@storybook/addon-styling',
-    options: {
-      // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-      // For more details on this addon's options.
-      postCss: {
-        implementation: require.resolve('postcss'),
-      },
+    stories: [
+        '../src/app/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        {
+            name: path.dirname(require.resolve('@storybook/addon-docs/package.json')),
+            options: { transcludeMarkdown: true },
+        },
+        {
+            name: '@storybook/addon-essentials',
+            options: { docs: false },
+        },
+        '@storybook/addon-interactions',
+        '@storybook/addon-docs',
+        {
+            name: '@storybook/addon-styling',
+            options: {
+                // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+                // For more details on this addon's options.
+                postCss: {
+                    implementation: require.resolve('postcss'),
+                },
+            },
+        },
+        {
+            name: '@storybook/addon-styling',
+            options: {},
+        },
+    ],
+    framework: {
+        name: '@storybook/angular',
+        options: {},
     },
-  }, {
-    name: '@storybook/addon-styling',
-    options: {},
-  }, '@storybook/addon-styling-webpack'],
-  framework: {
-    name: '@storybook/angular',
-    options: {},
-  },
-  staticDirs: [{ from: './../src/assets', to: '/assets' }],
-  docs: {
-    autodocs: 'tag',
-    defaultName: "DOCS"
-  },
+    staticDirs: [{ from: './../src/assets', to: '/assets' }],
+    docs: {
+        autodocs: 'tag',
+    },
 };
 
 export default config;
 
 // To customize your webpack configuration you can use the webpackFinal field.
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
-// and https://nx.dev/recipes/storybook/custom-builder-configs
+// and https://nx.dev/packages/storybook/documents/custom-builder-configs
