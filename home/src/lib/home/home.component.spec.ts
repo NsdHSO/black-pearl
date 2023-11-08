f2296883ddead9b4f6fd551801cab112e22c4bdd
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-11-08 02:08:06 +0200

    chore:🌻 play with tests

--- home/src/lib/home/home.component.spec.ts
+++ home/src/lib/home/home.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import {provideMockStore} from "@ngrx/store/testing";
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,14 +9,34 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
+      providers: [
+          provideMockStore()
+      ]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
   });
 
   it('should create', () => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+  it('should not have touched', () => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    const input: HTMLInputElement = fixture.nativeElement.querySelector("[data-test='control']")
+    expect(input.classList).toContain('ng-valid')
+  });
+  it('should not have touched', () => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    component.name.setValue('ivan')
+    fixture.detectChanges();
+    const input: HTMLInputElement = fixture.nativeElement.querySelector("[data-test='control']")
+    expect(input.classList).toContain('ng-untouched')
+    expect(input.value).toContain("ivan")
   });
 });
--- home/src/lib/home/home.component.ts
+++ home/src/lib/home/home.component.ts
@@ -2,16 +2,17 @@ import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {select, Store} from "@ngrx/store";
 import {selectHomeState} from "../+state/home.selectors";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'black-pearl-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  store = inject(Store)
+  store = inject(Store);
 
-  store$ = this.store.pipe(select(selectHomeState))
+  name = new FormControl('test');
 }
