0a0f7d84ddb15f1a46a83000628fcd0fd9603eff
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-11-11 11:31:50 +0200

    feat:🎸 migrate to angular 17

--- synergy/src/lib/synergy/components/input-text/inputText.component.ts
+++ synergy/src/lib/synergy/components/input-text/inputText.component.ts
@@ -1,23 +1,16 @@
-import {ChangeDetectionStrategy, Component, forwardRef, Input} from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  ControlValueAccessor, FormsModule,
-  NG_VALUE_ACCESSOR,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import {OutlineDirective} from "../../directive/outline/outline.directive";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { OutlineDirective } from '../../directive/outline/outline.directive';
 import { AbstractAccessor, FORWARD_REF } from '../../utils';
+
 @Component({
   selector: 'black-pearl-input-text',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './inputText.component.html',
   styleUrls: ['./inputText.component.scss'],
-  providers: [
-    FORWARD_REF(InputTextComponent)
-  ],
-  hostDirectives: [{directive:OutlineDirective}],
+  providers: [FORWARD_REF(InputTextComponent)],
+  hostDirectives: [{ directive: OutlineDirective }],
 })
-export class InputTextComponent extends AbstractAccessor{
-
-}
+export class InputTextComponent extends AbstractAccessor {}
