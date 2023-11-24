1db49af741eb823a317ed4c3938f943569166ffc
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-11-25 00:41:45 +0200

    chore:🌻 add husky hook

--- synergy/src/lib/synergy/utils/interfaces/accounts.type.ts
+++ synergy/src/lib/synergy/utils/interfaces/accounts.type.ts
@@ -1,20 +1,22 @@
-import { Icon } from './icon.type';
+import {Icon} from './icon.type';
 
 export interface Account {
-  IBAN: string;
-  currency: string;
-  amount: number;
-  icon: Icon;
-  configuration?: ConfigurationAccount;
+    IBAN: string;
+    currency: string;
+    amount: number;
+    icon: Icon;
+    configuration?: ConfigurationAccount;
 }
 
 export interface ConfigurationAccount {
-  buttons: ConfigButton[];
+    buttons: ConfigButton[];
 }
 
 export interface ConfigButton {
-  visible: true;
-  name: string;
-  action: (event: unknown) => void;
-  IBAN: string;
+    visible: true;
+    name: string;
+    action: (event: unknown) => void;
+    IBAN: string;
 }
+
+¥
