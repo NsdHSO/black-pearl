f1a66ea348cc6cfc63d5a5dd78d60216991f1619
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-12-20 02:04:22 +0200

    r 🦺 : functional programming

--- graph/src/lib/graph/util/helpers/generate.ts
+++ graph/src/lib/graph/util/helpers/generate.ts
@@ -1,26 +1,42 @@
 import * as d3 from 'd3';
 
-let svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>;
+let svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, never>;
 const width = 400;
 const height = 600;
 
 export function buildSVG(container: string) {
   if (!svg) {
     svg = d3.select(container).append('svg').classed('bar-char', true);
-    buildContainerGroups();
+
+    const containerGroup = svg
+      .append('g')
+      .classed('container-group', true)
+      .attr('transform', `translate(20, 10)`);
+
+    buildContainerGroups(containerGroup);
   }
 
   svg.attr('width', width);
   svg.attr('height', height);
 }
 
-function buildContainerGroups() {
-  const container = svg
-    .append('g')
-    .classed('container-group', true)
-    .attr('transform', `translate(20, 10)`);
+function generateG(
+  container: d3.Selection<SVGGElement, unknown, HTMLElement, never>,
+  classes: string,
+  showed: boolean,
+) {
+  container.append('g').classed(classes, showed);
+}
 
-  container.append('g').classed('chart-axis-group', true);
-  container.append('g').classed('y-axis-group axis', true);
-  container.append('g').classed('x-axis-group axis', true);
+function buildContainerGroups(
+  container: d3.Selection<SVGGElement, unknown, HTMLElement, never>,
+) {
+  buildContainer(container);
+}
+
+function buildContainer(
+  container: d3.Selection<SVGGElement, unknown, HTMLElement, never>,
+) {
+  generateG(container, 'y-axis-group axis', true);
+  generateG(container, 'x-axis-group axis', true);
 }
